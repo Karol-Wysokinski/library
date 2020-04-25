abf9c0c9f5804334b554c00fa6e7cec721125a29
Karol Wysokiński <k.j.wysokinski@gmail.com>
2020-04-25 10:29:23 +0200

    initial UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner SCANNER;
+
     public static void main(String[] args) {
-        System.out.println("Robimy bibliotekę");
+
+        System.out.println("Witaj w bibliotece! Co mogę dla ciebie zrobić?");
+        SCANNER = new Scanner(System.in);
+
+
+        String jeden = "1) Wyświetl wszystkie książki";
+        String dwa = "2) Dodaj książkę";
+        String trzy = "3) Usuń książkę";
+
+
+        while (true) {
+            menu();
+            int option = SCANNER.nextInt();
+
+            switch (option) {
+
+
+                case 1:
+                    System.out.println(jeden);
+                    break;
+                case 2:
+                    System.out.println(dwa);
+                    break;
+                case 3:
+                    System.out.println(trzy);
+                    break;
+                default:
+                    System.out.println("Operacja o tym numerze jeszcze nie istnieje");
+
+
+            }
+
+
+        }
+        private static void menu() {
+            System.out.println("1) Wyświetl wszystkie książki");
+            System.out.println("2) Dodaj książkę");
+            System.out.println("3) Usuń książkę");
+            System.out.println("Wybieram: ");
+        }
     }
 }
+
